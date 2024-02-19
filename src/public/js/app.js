01bdc94273e6f941d15cfe962550b301f7a69f78
0425kid <0425kid@gmail.com>
2024-02-19 15:01:01 +0900

    init

---
+++ src/public/js/app.js
@@ -0,0 +1,7 @@
+console.log("Rikka's the best")
+var result = confirm("릿카는 가장 귀엽습니까?");
+if(result){
+    alert("당연하죠! 역시 릿카..");
+}else{
+    alert("뭘 모르시네..");
+}
---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+console.log("hello");
+
+app.get('/', (req, res)=>{
+    res.render("home")
+})
+
+const handleListen = () => console.log(`Listening on http`);
+
+app.listen(3000, handleListen);
